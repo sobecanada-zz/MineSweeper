58eccbdd3ebac6fb6ea22be42f5c12fbbe0378ed
oliver h <ohuang712@gmail.com>
2015-01-29 00:19:48 -0800

    winner/game-over check, flag function done. Still need upper/lower case alphabet exception and AttuributeError exception needed

--- Python/minesweeper.py
+++ Python/minesweeper.py
@@ -5,9 +5,9 @@ import time
 from string import ascii_lowercase
 
 #Setting up a defualt grid
-def setUpGrid(grid_size, start, number_of_mines):
+def setUpGrid(grid_size, number_of_mines):
     default_grid = [['0' for i in range(grid_size) ] for i in range(grid_size)]
-    mines = getMines(default_grid, start, number_of_mines)
+    mines = getMines(default_grid, number_of_mines)
 
     for i, j in mines:
         default_grid[i][j] = 'X'
@@ -39,7 +39,7 @@ def displayGrid(grid):
     print('')
 
 #Get mines
-def getMines(grid, start, number_of_mines):
+def getMines(grid, number_of_mines):
     mines = []
 
     for i in range(number_of_mines):
@@ -81,56 +81,112 @@ def setNumbers(grid):
                 #Insert number of mines around the current location
                 grid[row_num][col_num] = str(values.count('X'))
     return grid
+
 #Reveal blocks (copying value from grid(ans) to game board for display)
-def revealBlocks(grid, game_board, row, col):
+def revealBlocks(opened_blocks, grid, game_board, row, col):
     #if the block has NOT been revealed already
     if game_board[row][col] == '?':
         game_board[row][col] = grid[row][col]
+	opened_blocks.append((row, col))
         if grid[row][col] == '0':
             for r, c in findNeighbours(grid, row, col):
-                revealBlocks(grid, game_board, r, c)
+                revealBlocks(opened_blocks, grid, game_board, r, c)
     else:
         return
 
 #Validate user input and return with proper coordinate and msg
-def validateInput(grid, input):
+def validateInput(grid, promp):
     grid_size = len(grid)
-    invalid_input_msg = 'Wrong Input Value'
+    invalid_input_msg = '\n >>>Wrong Input Value!! \n'
     coordinate = ()
 
     pattern = r'([a-{}])([0-9]+)(f?)'.format(ascii_lowercase[grid_size - 1])
-    valid_input = re.match(pattern, input)
-
-    if valid_input:
+    valid_input = re.match(pattern, promp)
+    
+    #If the input is validated with the regex and in the grid
+    if valid_input or int(valid_input.group(2)) <= (grid_size - 1):
         col_num = ascii_lowercase.index(valid_input.group(1))
         row_num = int(valid_input.group(2))
+	flag = valid_input.group(3)
         coordinate = (row_num, col_num)
         invalid_input_msg = ''
 
-    return {'coordinate': coordinate, 'msg': invalid_input_msg}
+    return {'coordinate': coordinate, 'flag':flag, 'msg': invalid_input_msg}
+
+#Promped Grid Size from User
+def prompedGridInfo():
+    grid_info = ()
+    try:
+	grid_size = input("Enter the size of the game board(just the width): ")
+	number_of_mines = input("Enter the number of mines(the number shoud be smaller than {}): ".format(grid_size ** 2))
+	if number_of_mines >= (grid_size ** 2):
+	    print "\n >>>Number of mines has to be smaller than the board size {}!! \nPLEASE TRY AGAIN".format(grid_size ** 2)
+	else:
+	    grid_info = (grid_size, number_of_mines)
+    except NameError:
+	print "\n >>>Please input an numeracal numbers(E.g. 1 ~ 99 )!! \nPLEASE TRY AGAIN"
+
+    return grid_info
 
 #Main Game
 def playGame():
-    grid_size = int(raw_input("Enter the size of the game board: "))
-    number_of_mines = int(raw_input("Enter the number of mines: "))
-    start = 0
-    grid, mines = setUpGrid(grid_size, start, number_of_mines)
+    start_time = 0
+    input_grid_info = ()
+    all_flags = []
+    
+    while len(input_grid_info) is 0:
+	input_grid_info = prompedGridInfo()
+    
+    grid_size, number_of_mines = input_grid_info
+    opened_blocks = []
+    grid, mines = setUpGrid(grid_size,  number_of_mines)
 
     game_board = [['?' for i in range(grid_size) ] for i in range(grid_size)]
     displayGrid(game_board)
 
-    Test = True
-    msg = ("Please enter the coordinate: ")
 
+    Test = True
+
+    msg = ("Please enter the coordinate (E.g. \"a0\" to revel the block or \"a0f\" for set/remove a flag): ")
     while Test == True:
-        input = raw_input(msg)
-        result = validateInput(grid, input)
+	start_time - time.time();
+        unopen_blocks = (grid_size ** 2 - len(opened_blocks) - number_of_mines)
+	
+	#Winner Check
+	if (unopen_blocks is 0) or (set(all_flags) == set(mines)):
+	    minues, seconds = divmod(int(time.time() - start_time), 60)
+	    print "\n\n	WINNER!!! You solved in {} minues, and {} seconds".format((minues % 60), seconds)
+	    return
+
+        print '"', unopen_blocks, '"', 'blocks left to open!!'
+        promp = raw_input(msg)
+        result = validateInput(grid, promp)
 
         input_coordinate = result['coordinate']
+	#Validate input(coordinate)
         if input_coordinate:
-
-             revealBlocks(grid, game_board, input_coordinate[0],
-                     input_coordinate[1])
-
-             displayGrid(game_board)
+	    #If the block has never being revealed
+	    if game_board[int(input_coordinate[0])][int(input_coordinate[1])] == '?': 
+		#If the user wants to put flag on the block
+		if result['flag']:
+		    #If the block already has a flag
+		    if grid[int(input_coordinate[0])][int(input_coordinate[1])] == 'F':
+			all_flags.remover(result['coordinate'])
+			game_board[int(input_coordinate[0])][int(input_coordinate[1])] = '?'
+		    else:
+			all_flags.append(result['coordinate'])
+			game_board[int(input_coordinate[0])][int(input_coordinate[1])] = 'F'
+			displayGrid(game_board)
+	        elif grid[int(input_coordinate[0])][int(input_coordinate[1])] == 'X':
+		    print '\n \n	GAME OVER!!! CHECK THE ANSWER BOARD BELOW ;)'
+		    displayGrid(grid)
+                    return
+		else:
+             	    revealBlocks(opened_blocks, grid, game_board, input_coordinate[0], 
+		    input_coordinate[1])
+		    displayGrid(game_board)
+	    else:
+		 print '\n >>>The block is already opened, please enter the new coordinate!! \nPLEASE TRY AGAIN'
+	else:
+	    print result['msg']	
 playGame()
